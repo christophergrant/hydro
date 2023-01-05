a2da6a3e3d3ed9b6e0e750dba7b519d47cf1c2cc
toph <christopher.grant@protonmail.com>
2023-01-04 20:23:07 -0800

    ok wings on this time hopefully

--- hydro/__init__.py
+++ hydro/__init__.py
@@ -4,14 +4,15 @@ import json
 
 from delta import DeltaTable
 from pyspark.sql import DataFrame
+from pyspark.sql.types import DataType
 from pyspark.sql.types import StructType
 
 # transformed numFiles to string, sizeInBytes -> size with type string
-DETAIL_SCHEMA_JSON = '{"fields":[{"metadata":{},"name":"createdAt","nullable":true,"type":"timestamp"},{"metadata":{},"name":"description","nullable":true,"type":"string"},{"metadata":{},"name":"format","nullable":true,"type":"string"},{"metadata":{},"name":"id","nullable":true,"type":"string"},{"metadata":{},"name":"lastModified","nullable":true,"type":"timestamp"},{"metadata":{},"name":"location","nullable":true,"type":"string"},{"metadata":{},"name":"minReaderVersion","nullable":true,"type":"long"},{"metadata":{},"name":"minWriterVersion","nullable":true,"type":"long"},{"metadata":{},"name":"name","nullable":true,"type":"string"},{"metadata":{},"name":"numFiles","nullable":true,"type":"string"},{"metadata":{},"name":"partitionColumns","nullable":true,"type":{"containsNull":true,"elementType":"string","type":"array"}},{"metadata":{},"name":"properties","nullable":true,"type":{"keyType":"string","type":"map","valueContainsNull":true,"valueType":"string"}},{"metadata":{},"name":"size","nullable":true,"type":"string"}],"type":"struct"}'
+DETAIL_SCHEMA_JSON = '{"fields":[{"metadata":{},"name":"createdAt","nullable":true,"type":"timestamp"},{"metadata":{},"name":"description","nullable":true,"type":"string"},{"metadata":{},"name":"format","nullable":true,"type":"string"},{"metadata":{},"name":"id","nullable":true,"type":"string"},{"metadata":{},"name":"lastModified","nullable":true,"type":"timestamp"},{"metadata":{},"name":"location","nullable":true,"type":"string"},{"metadata":{},"name":"minReaderVersion","nullable":true,"type":"long"},{"metadata":{},"name":"minWriterVersion","nullable":true,"type":"long"},{"metadata":{},"name":"name","nullable":true,"type":"string"},{"metadata":{},"name":"numFiles","nullable":true,"type":"string"},{"metadata":{},"name":"partitionColumns","nullable":true,"type":{"containsNull":true,"elementType":"string","type":"array"}},{"metadata":{},"name":"properties","nullable":true,"type":{"keyType":"string","type":"map","valueContainsNull":true,"valueType":"string"}},{"metadata":{},"name":"size","nullable":true,"type":"string"}],"type":"struct"}'  # noqa: E501
 
 
-def _humanize_bytes(num_bytes) -> str:
-    # ChatGPT 🤖 prompt: "write a python program that converts bytes to their proper units (kb, mb, gb, tb, pb, etc)"
+def _humanize_bytes(num_bytes: float) -> str:
+    # ChatGPT 🤖 prompt: "write a python program that converts bytes to their proper units (kb, mb, gb, tb, pb, etc)" # noqa: E501
     units = ['bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB']
     i = 0
     while num_bytes >= 1024 and i < len(units) - 1:
@@ -25,7 +26,8 @@ def _snapshot_allfiles(delta_table: DeltaTable) -> DataFrame:
     location = delta_table.detail().collect()[0]['location']
 
     delta_log = spark._jvm.org.apache.spark.sql.delta.DeltaLog.forTable(
-        spark._jsparkSession, location,
+        spark._jsparkSession,
+        location,
     )
     return delta_log.snapshot().allFiles()
 
@@ -54,19 +56,24 @@ def get_table_zordering(delta_table: DeltaTable) -> DataFrame:
 
 
 def fields(
-    delta_table: DeltaTable, include_types: bool = False,
-) -> list[str]:
+    delta_table: DeltaTable,
+    include_types: bool = False,
+) -> list[tuple[str, DataType] | str]:
     # ChatGPT 🤖 prompt:
-    # write a program that takes a PySpark StructType and returns the leaf node field names, even the nested ones
+    # write a program that takes a PySpark StructType and returns the leaf node field names, even the nested ones # noqa: E501
     schema = delta_table.toDF().schema
 
-    def get_leaf_fields(struct_type, include_types):
-        def _get_leaf_fields(struct_type, prefix):
-            fields = []
-            for field in struct_type:
+    def get_leaf_fields(
+        struct: StructType, include_types: bool,
+    ) -> list[tuple[str, DataType] | str]:
+        def _get_leaf_fields(
+            struct: StructType, prefix: str,
+        ) -> list[tuple[str, DataType] | str]:
+            fields: list[tuple[str, DataType] | str] = []
+            for field in struct:
                 if isinstance(field.dataType, StructType):
                     fields.extend(
-                        _get_leaf_fields(field.dataType, prefix + field.name + '.'),
+                        _get_leaf_fields(field.dataType, prefix + field.name + '.'),  # noqa: E501
                     )
                 else:
                     if include_types:
@@ -75,6 +82,6 @@ def fields(
                         fields.append(prefix + field.name)
             return fields
 
-        return _get_leaf_fields(struct_type, '')
+        return _get_leaf_fields(struct, '')
 
     return get_leaf_fields(schema, include_types)
